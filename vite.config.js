c4267ff06316588d20b57805392bcbc48f4d5000
mirimachado <mirii.machadoo.c@gmail.com>
2025-03-10 10:22:21 -0300

    modifications - vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-photography-project/', // 🛑 Coloque o nome exato do repositório aqui
+  base: '/portfolio-photography-project/', 
 });
